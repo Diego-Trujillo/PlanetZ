f77b27eaeb0d3813274085f7921325edbaee9e71
Diego-Trujillo <diego.tn94@gmail.com>
2015-11-19 12:42:50 -0600

    Modifiquè cosas

--- app/src/main/java/mx/itesm/planetz/AdventureLevelTwoScene.java
+++ app/src/main/java/mx/itesm/planetz/AdventureLevelTwoScene.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 
+import org.andengine.engine.handler.IUpdateHandler;
+import org.andengine.engine.handler.UpdateHandlerList;
 import org.andengine.engine.handler.timer.ITimerCallback;
 import org.andengine.engine.handler.timer.TimerHandler;
 import org.andengine.entity.primitive.Rectangle;
@@ -15,6 +17,8 @@ import org.andengine.extension.physics.box2d.PhysicsConnector;
 import org.andengine.extension.physics.box2d.PhysicsFactory;
 import org.andengine.extension.physics.box2d.PhysicsWorld;
 import org.andengine.input.touch.TouchEvent;
+
+import java.util.Random;
 
 /**
  * Created by Diego on 16/11/2015.
@@ -44,7 +48,7 @@ public class AdventureLevelTwoScene extends BaseScene{
     // -- Paredes laterales
     final FixtureDef WALL_FIXTURE_DEFINITION = PhysicsFactory.createFixtureDef(0,0f,0f);
     // -- Nave
-    final FixtureDef SHIP_FIXTURE_DEFINITION = PhysicsFactory.createFixtureDef(10.f,0.1f,0.5f);
+    final FixtureDef SHIP_FIXTURE_DEFINITION = PhysicsFactory.createFixtureDef(1000.f,0.1f,0.5f);
     //-- Meteoros
     final FixtureDef METEOR_FIXTURE_DEFINITION = PhysicsFactory.createFixtureDef(1f,0.9f,0.4f);
 
@@ -56,6 +60,8 @@ public class AdventureLevelTwoScene extends BaseScene{
     // -- "Pared de la muerte"
     private Body wallOfDeathBody;
 
+    Random rand;
+    private int i= 1;
 
     private Rectangle astronautRectangle;
     private Body astronautBody;
@@ -86,21 +92,22 @@ public class AdventureLevelTwoScene extends BaseScene{
         createWalls();
 
         createAstronaut();
-
-        TimerHandler platformSpawner = new TimerHandler(3f,true, new ITimerCallback() {
+        rand = new Random();
+        TimerHandler platformSpawner = new TimerHandler(1,true, new ITimerCallback() {
             @Override
             public void onTimePassed(TimerHandler pTimerHandler) {
-                final Rectangle rekt= new Rectangle(GameManager.CAMERA_WIDTH,200,250,50, vertexBufferObjectManager);
+                final Rectangle rekt= new Rectangle(GameManager.CAMERA_WIDTH * i ,40*rand.nextFloat() ,GameManager.CAMERA_HEIGHT*1.25F,50, vertexBufferObjectManager);
                 rekt.setColor(1f, 0, 1f);
                 Body rektBody = PhysicsFactory.createBoxBody(physicsWorld, rekt, BodyDef.BodyType.KinematicBody, WALL_FIXTURE_DEFINITION);
 
                 physicsWorld.registerPhysicsConnector(new PhysicsConnector(rekt, rektBody));
 
                 attachChild(rekt);
-
-                rektBody.setLinearVelocity(-5,0);
+                i++;
+                //rektBody.setLinearVelocity(-5,0);
             }
         });
+
 
         this.registerUpdateHandler(platformSpawner);
 
@@ -151,17 +158,17 @@ public class AdventureLevelTwoScene extends BaseScene{
 
         astronautBody = PhysicsFactory.createBoxBody(physicsWorld, astronautRectangle, BodyDef.BodyType.DynamicBody, SHIP_FIXTURE_DEFINITION);
 
-        physicsWorld.registerPhysicsConnector(new PhysicsConnector(astronautRectangle,astronautBody));
+        physicsWorld.registerPhysicsConnector(new PhysicsConnector(astronautRectangle, astronautBody));
 
         this.attachChild(astronautRectangle);
 
 
-        Sprite sp = new Sprite(500,500, resourceManager.loadImage("Graphics/Menu/BackArrow.png"),vertexBufferObjectManager){
+        Sprite sp = new Sprite(150,100, resourceManager.loadImage("Graphics/Menu/BackArrow.png"),vertexBufferObjectManager){
             @Override
             public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float X, float Y){
                 if (pSceneTouchEvent.isActionDown()) {
                     System.out.println("is touched");
-                    astronautBody.applyForce(0, astronautBody.getMass() * 1450, astronautBody.getWorldCenter().x, astronautBody.getWorldCenter().y);
+                    astronautBody.setLinearVelocity(astronautBody.getLinearVelocity().x,15);
 
                 }
                 return true;
@@ -169,8 +176,41 @@ public class AdventureLevelTwoScene extends BaseScene{
 
         };
 
-        this.attachChild(sp);
+
+        Sprite sp2 = new Sprite(300,100, resourceManager.loadImage("Graphics/Menu/BackArrow.png"),vertexBufferObjectManager){
+            @Override
+            public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float X, float Y){
+                if (pSceneTouchEvent.isActionDown()) {
+                    System.out.println("is touched");
+                    astronautBody.setLinearVelocity(5,0);
+
+                }
+                return true;
+            }
+
+        };
+        sp.setRotation(90f);
+       //this.attachChild(sp);
         this.registerTouchArea(sp);
+        this.registerTouchArea(sp2);
+        astronautRectangle.attachChild(sp);
+        astronautRectangle.attachChild(sp2);
+
+        astronautRectangle.registerUpdateHandler(new IUpdateHandler() {
+            @Override
+            public void onUpdate(float pSecondsElapsed) {
+                astronautBody.setLinearVelocity(25f,astronautBody.getLinearVelocity().y);
+                astronautRectangle.setRotation(0);
+                astronautBody.setAngularVelocity(0);
+            }
+
+
+            @Override
+            public void reset() {
+
+            }
+        });
+        camera.setChaseEntity(astronautRectangle);
 
 
     }
